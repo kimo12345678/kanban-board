40af14d6b7eab7558265964ec827e091f0a10f5a
kimo7520 <karim.saad2327@yahoo.com>
2024-11-12 20:02:46 +0200

    feat: added button to edit and delete and move across boards

--- kanban-board/src/components/KanbanCard.tsx
+++ kanban-board/src/components/KanbanCard.tsx
@@ -1,5 +1,4 @@
-// src/components/KanbanCard.tsx
-import React from 'react';
+import React, { useState } from "react";
 
 interface Member {
   name: string;
@@ -7,15 +6,37 @@ interface Member {
   age: number;
   email: string;
   mobileNumber: string;
-  status: 'Unclaimed' | 'First Contact' | 'Preparing Work Offer' | 'Send to Therapist';
+  status:
+    | "Unclaimed"
+    | "First Contact"
+    | "Preparing Work Offer"
+    | "Send to Therapist";
 }
 
 interface KanbanCardProps {
   member: Member;
-  onUpdateMemberStatus: (member: Member, newStatus: Member['status']) => void;
+  onUpdateMemberStatus: (member: Member, newStatus: Member["status"]) => void;
+  onEdit: (member: Member) => void;
+  onDelete: (email: string) => void;
 }
 
-const KanbanCard: React.FC<KanbanCardProps> = ({ member }) => {
+const statuses: Member["status"][] = [
+  "Unclaimed",
+  "First Contact",
+  "Preparing Work Offer",
+  "Send to Therapist",
+];
+
+const KanbanCard: React.FC<KanbanCardProps> = ({
+  member,
+  onUpdateMemberStatus,
+  onEdit,
+  onDelete,
+}) => {
+  const handleStatusChange = (newStatus: Member["status"]) => {
+    onUpdateMemberStatus(member, newStatus);
+  };
+
   return (
     <div className="bg-white p-4 rounded-lg shadow-md flex flex-col">
       <h3 className="font-semibold text-gray-800">
@@ -24,6 +45,34 @@ const KanbanCard: React.FC<KanbanCardProps> = ({ member }) => {
       <p className="text-gray-600">{member.email}</p>
       <p className="text-gray-600">{member.mobileNumber}</p>
       <p className="text-gray-600">{member.age} yo</p>
+      <div className="mt-4 flex gap-2 flex-wrap">
+        {statuses.map(
+          (status) =>
+            status !== member.status && (
+              <button
+                key={status}
+                className="bg-blue-500 text-white w-24 h-8 text-xs rounded-md hover:bg-blue-600 shadow-sm transition duration-200 ease-in-out flex items-center justify-center"
+                onClick={() => handleStatusChange(status)}
+              >
+                Move to {status}
+              </button>
+            )
+        )}
+      </div>
+      <div className="mt-2 flex gap-2">
+        <button
+          className="bg-green-500 text-white py-1 px-2 rounded-md hover:bg-green-600"
+          onClick={() => onEdit(member)}
+        >
+          Edit
+        </button>
+        <button
+          className="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600"
+          onClick={() => onDelete(member.email)}
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
